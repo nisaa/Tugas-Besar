2947e5eeea12b903349ecaa65db07207f070d211
annisa <miegoreng10@gmail.com>
2014-01-31 06:09:41 +0700

    Fungsi TSP

--- main.c
+++ main.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 
 #define MAX 100
+#define INF 999
+ 
+int TSP(int n, int matriks[][MAX], int rute[], int awal)
+{
+    int temp[MAX], rute_minimum[MAX]; 
+    int jarak_terpendek, jarak;
+    int i, j, k;
+    
+    if(awal == n - 2)
+        return matriks[rute[n - 2]][rute[n - 1]] + matriks[rute[n - 1]][0];
+ 
+    jarak_terpendek = INF;
+    
+    for(i = awal + 1;i < n;i++)
+    {
+        for(j = 0;j < n;j++)
+            temp[j] = rute[j];
+ 
+        temp[awal + 1] = rute[i];
+        temp[i] = rute[awal + 1];
+
+        if(matriks[rute[awal]][rute[i]] + (jarak = TSP(n, matriks, temp, awal + 1)) < jarak_terpendek) 
+        {
+            jarak_terpendek = matriks[rute[awal]][rute[i]] + jarak;
+            for(k = 0;k < n;k++)
+                rute_minimum[k] = temp[k];
+        }
+    }
+
+    for(i = 0;i < n;i++)
+        rute[i] = rute_minimum[i];
+
+    return jarak_terpendek;
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,11 +54,7 @@ int main(int argc, char *argv[])
     for(i = 0;i < jumlah_kota;i++)
         rute[i] = i;
 
-    jarak_minimum = 0;
-
-    /*
-        Proses TSP
-    */
+    jarak_minimum = TSP(jumlah_kota, matriks, rute, 0);
 
     printf("Jarak Minimum: %i\n", jarak_minimum);
 
